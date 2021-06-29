9dfaf8a8f1d266cc015041ac1e41232488b7942d
“dongyuekai” <457545648@qq.com>
2021-06-29 16:59:11 +0800

    折线图点连线

--- 02-myCharts/02-myCharts-config/src/charts.js
+++ 02-myCharts/02-myCharts-config/src/charts.js
@@ -1,7 +1,7 @@
 import utils from './utils'
 import myAnimation from './myAnimation'
 import Circle from './circle'
-import { drawAxis, drawPoint } from './broken'
+import { drawAxis, drawPoint, drawBrokenLine } from './broken'
 
 class MyCharts {
   constructor(defaultParam) {
@@ -77,6 +77,8 @@ class MyCharts {
           render: (current) => {
             // 绘制坐标系
             drawAxis.call(this)
+            // 绘制虚线
+            drawBrokenLine.call(this, current / 200)
             // 绘制圆形
             drawPoint.call(this, current / 200)
           }
